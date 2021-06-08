bbcfd8d9560ae53539079f5d01740ef2f39d3d81
NguyenXuanLoc <59056264+NguyenXuanLoc@users.noreply.github.com>
2021-06-08 10:15:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sip_test2/Test_webview.java
@@ -0,0 +1,14 @@
+package com.example.sip_test2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Test_webview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_webview);
+    }
+}
